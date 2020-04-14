7cb1ddb2dd44cc444fc035fd10455ab2fc42fac8
Talon24 <talontalon24@gmail.com>
2020-04-14 13:28:34 +0200

    refine post method, load_contacts gets IMs, channel shoud be preceded with # if channel, else IM

--- kettle-rocketchat-step/src/main/java/org/kettle/rocketchat/RocketchatClient.java
+++ kettle-rocketchat-step/src/main/java/org/kettle/rocketchat/RocketchatClient.java
@@ -10,8 +10,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+import org.json.JSONArray;
 //import org.json.JSONArray;
 import org.json.JSONObject;
+
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonObjectFormatVisitor;
 
 public class RocketchatClient {
 	private String charset = java.nio.charset.StandardCharsets.UTF_8.name();
@@ -46,17 +49,39 @@ public class RocketchatClient {
 	
 	public JSONObject send_message(String receiver, String content, String alias, String emoji) throws MalformedURLException, IOException {
 		HashMap<String, String> payload = new HashMap<String, String>();
-		payload.put("roomId", contacts.get(receiver));
 		payload.put("text", content);
 		payload.put("alias", alias);
 		payload.put("emoji", emoji);
-		return post_message(payload);
+//		System.out.println("Contacts:" );
+//		System.out.println(contacts.toString());
+		if (receiver.startsWith("#")) {
+			payload.put("channel", receiver);
+		}
+		else {
+			if (!contacts.containsKey(receiver)) {
+				create_im(receiver);
+			}
+			payload.put("roomId", contacts.get(receiver));
+		}
+		JSONObject result = post_message(payload);
+//		System.out.println(result.toString(4));
+//		System.out.println("Contacts after send:" );
+//		System.out.println(contacts.toString());
+		return result;
 	}
 	
 	private JSONObject post_message(Map<String, String> payload) throws MalformedURLException, IOException {
-		JSONObject result = post("chat.postMessage", new JSONObject(payload).toString());
+		JSONObject result = post("chat.postMessage", payload);
 //		System.out.println(result);
 		return result;
+	}
+	
+	private void create_im(String user) throws MalformedURLException, IOException {
+		HashMap<String, String> payload = new HashMap<String, String>();
+		payload.put("username", user);
+		JSONObject result = post("im.create", payload);
+		String key = result.getJSONObject("room").getString("_id");
+		contacts.put(user, key);
 	}
 	
 	private void login() throws MalformedURLException, IOException {
@@ -65,29 +90,46 @@ public class RocketchatClient {
 		Map<String, String> payload = new HashMap<String, String>();
 		payload.put("user", user);
 		payload.put("password", password);
-		JSONObject result = post("login", new JSONObject(payload).toString());
-		System.out.println(result.toString(4));
-		System.out.println(result.getString("status") == "success");
+		JSONObject result = post("login", payload);
+//		System.out.println(result.toString(4));
+//		System.out.println(result.getString("status") == "success");
 	    String token = result.getJSONObject("data").getString("authToken");
 	    String userid = result.getJSONObject("data").getString("userId");
 	    headers.put("X-Auth-Token", token);
 	    headers.put("X-User-Id", userid);
 	}
-	
+
 	private void load_contacts() throws MalformedURLException, IOException { 
 	
-		JSONObject channels = get("channels.list");
-		JSONObject groups = get("groups.list");
-		for (Object channel : channels.getJSONArray("channels")) {
-			JSONObject obj = (JSONObject) channel;
-			contacts.put(obj.getString("name"), obj.getString("_id"));
-		}
-		for (Object channel : groups.getJSONArray("groups")) {
-			JSONObject obj = (JSONObject) channel;
-			contacts.put(obj.getString("name"), obj.getString("_id"));
+		JSONObject channels = get("im.list");
+		for (Object im: channels.getJSONArray("ims")) {
+			JSONObject obj = (JSONObject) im;
+			JSONArray names = obj.getJSONArray("usernames");
+			String name = null;
+			if (!names.get(0).equals(user)) {
+				name = names.getString(0);
+			} else {
+				name = names.getString(1);
+			}
+			contacts.put(name, obj.getString("_id"));
 		}
 		
 	}
+	
+//	private void load_contacts() throws MalformedURLException, IOException { 
+//	
+//		JSONObject channels = get("channels.list");
+//		JSONObject groups = get("groups.list");
+//		for (Object channel : channels.getJSONArray("channels")) {
+//			JSONObject obj = (JSONObject) channel;
+//			contacts.put(obj.getString("name"), obj.getString("_id"));
+//		}
+//		for (Object channel : groups.getJSONArray("groups")) {
+//			JSONObject obj = (JSONObject) channel;
+//			contacts.put(obj.getString("name"), obj.getString("_id"));
+//		}
+//		
+//	}
 	
 	private JSONObject get(String endpoint) throws MalformedURLException, IOException {
 		URLConnection connection = new URL(url + endpoint).openConnection();
@@ -123,7 +165,7 @@ public class RocketchatClient {
 //		}
 //	}
 
-	private JSONObject post(String endpoint, String payload) throws MalformedURLException, IOException {
+	private JSONObject post(String endpoint, Map<String, String> payload) throws MalformedURLException, IOException {
 		URLConnection connection = new URL(url + endpoint).openConnection();
 		connection.setRequestProperty("X-Auth-Token", headers.get("X-Auth-Token"));
 		connection.setRequestProperty("X-User-Id", headers.get("X-User-Id"));
@@ -134,8 +176,9 @@ public class RocketchatClient {
 //			connection.setRequestProperty(element.getKey(), element.getValue());
 //		}
 		connection.setDoOutput(true); // Triggers POST.
+		String payload_string = new JSONObject(payload).toString();
 		try(OutputStream os = connection.getOutputStream()) {
-		    byte[] input = payload.getBytes("utf-8");
+		    byte[] input = payload_string.getBytes("utf-8");
 		    os.write(input, 0, input.length);     
 		    os.flush();
 		    os.close();
